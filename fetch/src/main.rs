291915f57ec84205ed0825d276528f4fb3893ef6
Charles Bayley <cbayley95@gmail.com>
2021-09-07 15:20:44 -0400

    Got data into and back out of the database

--- fetch/src/main.rs
+++ fetch/src/main.rs
@@ -1,11 +1,19 @@
+use futures::stream::TryStreamExt;
 use mongodb::{
     Client,
-    options::ClientOptions
+    bson::{
+        Document, 
+        doc,
+    },
+    options::{
+        ClientOptions,
+        FindOptions,
+    },
 };
 use pokerust::{
     Berry,
     FromName,
-    Pokemon
+    Pokemon,
 };
 use std::error::Error;
 use tokio;
@@ -13,18 +21,33 @@ use tokio;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let berry = Berry::from_name("cheri").unwrap();
-    println!("{}: {}", berry.name, berry.max_harvest);
-
     let pokemon = Pokemon::from_name("pikachu").unwrap();
-    println!("{}: {}", pokemon.name, pokemon.base_experience);
 
     let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
     client_options.app_name = Some("Pokedex".to_string());
 
     let client = Client::with_options(client_options)?;
 
-    for db_name in client.list_database_names(None, None).await? {
-        println!("{}", db_name);
+    let pokedex_database = client.database("pokedex");
+    let pokemon_collection = pokedex_database.collection("pokemon");
+
+    let pokemon_doc = doc! {
+        "id": i32::from(pokemon.id),
+        "name": pokemon.name,
+    };
+
+    pokemon_collection.insert_one(pokemon_doc, None).await?;
+
+    let filter = doc! { "id": 25 };
+    let find_options = FindOptions::builder()
+        .sort(doc! { "name": 1 })
+        .build();
+
+    let cursor = pokemon_collection.find(filter, find_options).await?;
+    let results: Vec<Document> = cursor.try_collect().await.unwrap();
+
+    for result in results.iter() {
+        println!("{}", *result);
     }
 
     Ok(())
